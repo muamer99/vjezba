785400befa656b5d7e094096b1b5baa16d19efcc
muamer99 <154254558+muamer99@users.noreply.github.com>
2024-02-16 15:48:49 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
-const express = require('express');
-var cors = require('cors');
-const app = express();
-const propertiesRouter = require('./routes/properties');
+const showObjaviOglasOnClick = () => {
+    document.getElementById("modal").style.display = "block";
+}
 
-app.use(cors());
-const port = 3000;
+const hideObjaviOglas = () => {
+    document.getElementById("modal").style.display = "none";
+}
 
-app.use(express.json());
+const loadArticleGrid = async () => {
+    const articleGridId = "grid-list"
+    const gridListElement = document.getElementById(articleGridId);
 
-app.get('/', (req, res) => {
-    res.json({'message': 'ok'});
-});
+    const products = await (await fetch('http://localhost:3000/products',{
+        method: "GET"
+    })).json();
 
-app.use("/properties",propertiesRouter);
+    const htmlElements = products.map(item => {
+        const link = `./product/index.html?id=${item.id}`
+        return `
+        
+        <li>
+            <a href="${link}">
+                <card>
+                    <img src="${item.imageURL || "../images/items/placeholder.png"}" />
+                    <div class="card-body">
+                        <h1>
+                        ${item.title}
+                        </h1>
+                        <span class="tag">Novo</span>
+                        <div class="card-end">
+                        <small>Prije 20 sati</small>
+                        <span><b>${item.price} KM</b></span>
+                        </div>
+                    </div>
+                </card>
+            </a>
+        </li>
+    `
+    })
 
-/* Error Handler middleware */
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    console.error(err.message, err.stack);
-    res.status(statusCode).json({'massage': err.message});
+    gridListElement.innerHTML = htmlElements.join("");
 
-    return;
-});
+}
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+const loadTestItem = async () => {
+    const products = await (await
+        fetch('http://localhost:3000/properties', {
+            method: "GET"
+        })).json();
 
-app.use('/images', express.static('Public'));
+        console.log(products)
+
+        const item = `
+        <li>
+            <a href="item">
+                <img src="../images/items/placeholder.png" alt="plejsholder">
+                 <div class="card-body">
+                    <h1>Heading Lorem ipsum, dolor </h1>
+                    <span class="tag">Novo</span>
+                    <div class="card-end">
+                        <small>Prije 20 sati</small>
+                        <span><b>200,20 KM</b></span>
+                    </div>
+                </div>
+            </a>
+        </li>`
+        const gridList = document.getElementById("grid-list");
+        gridList.innerHTML = item
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadArticleGrid();
+}, false);
+
+document.addEventListener('DOMContentLoaded', async () => {
+    loadTestItem();
+    hideObjaviOglas();
+},false);
